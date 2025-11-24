4d2ef5d67871d8222acc0fdee63401037023bd3c
Luan Massuda <luanmassuda21@gmail.com>
2025-11-24 09:59:25 -0300

    adicionei listen em server.js e start no package.json

--- server.js
+++ server.js
@@ -100,6 +100,7 @@ client.connect();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
 
 app.use(cors());
 app.use(express.json());
